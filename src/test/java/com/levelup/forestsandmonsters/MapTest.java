1c98be0194c73987aa4a3c095f55e9c688c37179
Alex Vasconcelos <jose.a.vasconcelos@wellsfargo.com>
2022-09-15 19:05:36 +0000

    Added unit tests for calculate position

--- src/test/java/com/levelup/forestsandmonsters/MapTest.java
+++ src/test/java/com/levelup/forestsandmonsters/MapTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.lang.reflect.Executable;
+import java.awt.Point;
 
 import org.junit.Test;
 
@@ -44,5 +45,38 @@ public class MapTest {
         
         assertNull(map, "Unable to create map with negative dimension");
         } catch (Exception e) {}
+
+    }
+
+    @Test
+    public void calculatePositionWorks() {
+
+        Point p = new Point(1,1);
+        Map map = new Map();
+
+        Point end = map.calculatePosition(p, GameController.DIRECTION.NORTH);
+        assertEquals(new Point(1,2), end);
+        end = map.calculatePosition(p, GameController.DIRECTION.SOUTH);
+        assertEquals(new Point(1,0), end);
+        end = map.calculatePosition(p, GameController.DIRECTION.EAST);
+        assertEquals(new Point(2,1), end);
+        end = map.calculatePosition(p, GameController.DIRECTION.WEST);
+        assertEquals(new Point(0,1), end);
+
+        p = new Point(0,0);
+        end = map.calculatePosition(p, GameController.DIRECTION.NORTH);
+        assertEquals(new Point(0,1), end);
+        end = map.calculatePosition(p, GameController.DIRECTION.SOUTH);
+        assertEquals(new Point(0,0), end);
+        end = map.calculatePosition(p, GameController.DIRECTION.EAST);
+        assertEquals(new Point(1,0), end);
+        end = map.calculatePosition(p, GameController.DIRECTION.WEST);
+        assertEquals(new Point(0,0), end);
+
+    }
+    
+    @Test
+    public void validatePositionWorks() {
+
     }
 }
